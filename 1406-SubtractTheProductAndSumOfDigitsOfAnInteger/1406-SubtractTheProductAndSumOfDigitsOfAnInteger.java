e239cf39bd8f90c0be9cb71a88d10a516a3d283f
Abetha Senthalir <abethasenthalir@gmail.com>
2026-07-17 14:56:15 +0530

    Create 1406-SubtractTheProductAndSumOfDigitsOfAnInteger

---
+++ 1406-SubtractTheProductAndSumOfDigitsOfAnInteger/1406-SubtractTheProductAndSumOfDigitsOfAnInteger.java
@@ -0,0 +1,15 @@
+// Last updated: 7/17/2026, 2:56:14 PM
+class Solution {
+    public int subtractProductAndSum(int n) {
+        int product = 1;
+        int sum = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            product *= digit;
+            sum += digit;
+            n /= 10;
+        }
+        return product - sum;
+    }
+}
