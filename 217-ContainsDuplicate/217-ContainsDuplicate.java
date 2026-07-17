18b34d3278810f3dd5ed48eee8a39f09c052d1ba
Abetha Senthalir <abethasenthalir@gmail.com>
2026-07-17 14:58:01 +0530

    Create 217-ContainsDuplicate

---
+++ 217-ContainsDuplicate/217-ContainsDuplicate.java
@@ -0,0 +1,18 @@
+// Last updated: 7/17/2026, 2:58:00 PM
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                return true;
+            }
+            seen.add(num);
+        }
+
+        return false;
+    }
+}
