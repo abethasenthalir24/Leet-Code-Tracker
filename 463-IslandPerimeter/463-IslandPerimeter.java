c8190b888de1948ca02ea467678f6945ece84ba2
Abetha Senthalir <abethasenthalir@gmail.com>
2026-07-17 14:57:27 +0530

    Create 463-IslandPerimeter

---
+++ 463-IslandPerimeter/463-IslandPerimeter.java
@@ -0,0 +1,29 @@
+// Last updated: 7/17/2026, 2:57:26 PM
+class Solution {
+    public int islandPerimeter(int[][] grid) {
+        int perimeter = 0;
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+
+                if (grid[i][j] == 1) {
+                    perimeter += 4;
+
+          
+                    if (i > 0 && grid[i - 1][j] == 1) {
+                        perimeter -= 2;
+                    }
+
+                    // Check left neighbor
+                    if (j > 0 && grid[i][j - 1] == 1) {
+                        perimeter -= 2;
+                    }
+                }
+            }
+        }
+
+        return perimeter;
+    }
+}
