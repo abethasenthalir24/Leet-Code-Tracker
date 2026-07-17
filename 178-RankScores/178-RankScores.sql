62cd64c7ffeaf03d6e8d2261a119ce9d85acb940
Abetha Senthalir <abethasenthalir@gmail.com>
2026-07-17 14:58:17 +0530

    Create 178-RankScores

---
+++ 178-RankScores/178-RankScores.sql
@@ -0,0 +1,11 @@
+-- Last updated: 7/17/2026, 2:58:16 PM
+# Write your MySQL query statement below
+SELECT
+    s1.score,
+    (
+        SELECT COUNT(DISTINCT s2.score)
+        FROM Scores s2
+        WHERE s2.score >= s1.score
+    ) AS `rank`
+FROM Scores s1
+ORDER BY s1.score DESC;
