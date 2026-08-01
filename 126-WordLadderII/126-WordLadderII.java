d79160779bc0ce74677a143b32f4defcf807b396
Abetha Senthalir <abethasenthalir@gmail.com>
2026-08-01 07:25:58 +0530

    File updated at 8/1/2026, 7:25:57 AM

--- 126-WordLadderII/126-WordLadderII.java
+++ 126-WordLadderII/126-WordLadderII.java
@@ -1,38 +1,25 @@
-// Last updated: 8/1/2026, 7:20:37 AM
+// Last updated: 8/1/2026, 7:25:57 AM
 1class Solution {
-2    public List<List<Integer>> fourSum(int[] nums, int target) {
-3        List<List<Integer>> res = new ArrayList<>();
-4        Arrays.sort(nums);
-5        int n = nums.length;
-6
-7        for (int i = 0; i < n - 3; i++) {
-8            if (i > 0 && nums[i] == nums[i - 1]) continue;
-9
-10            for (int j = i + 1; j < n - 2; j++) {
-11                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
-12
-13                int left = j + 1;
-14                int right = n - 1;
-15
-16                while (left < right) {
-17                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
+2    public String countAndSay(int n) {
+3        String result = "1";
+4
+5        for (int i = 2; i <= n; i++) {
+6            StringBuilder sb = new StringBuilder();
+7            int count = 1;
+8
+9            for (int j = 1; j <= result.length(); j++) {
+10                if (j < result.length() && result.charAt(j) == result.charAt(j - 1)) {
+11                    count++;
+12                } else {
+13                    sb.append(count);
+14                    sb.append(result.charAt(j - 1));
+15                    count = 1;
+16                }
+17            }
 18
-19                    if (sum == target) {
-20                        res.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
-21                        left++;
-22                        right--;
-23
-24                        while (left < right && nums[left] == nums[left - 1]) left++;
-25                        while (left < right && nums[right] == nums[right + 1]) right--;
-26                    } else if (sum < target) {
-27                        left++;
-28                    } else {
-29                        right--;
-30                    }
-31                }
-32            }
-33        }
-34
-35        return res;
-36    }
-37}
+19            result = sb.toString();
+20        }
+21
+22        return result;
+23    }
+24}
